445fd089a3a7125ff29ac23e7ad9d9a4cb895400
vishalmet <vishalmet003@gmail.com>
2024-07-15 19:06:02 +0530

    home redirect

--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -1,17 +1,23 @@
 import HeadLogo from "@/assets/HeaderLogo.png";
 import Logo from "@/assets/logo.png";
 import Image from "next/image";
-
+import { useRouter } from "next/navigation";
 
 const HeaderLogo = () => {
+  const router = useRouter();
+
   return (
     <div>
-      <div className="pt-10">
-        <Image
-          src={HeadLogo}
-          alt="Header Logo"
-          className="mx-auto h-[36px] w-[228px]"
-        />
+      <div className="pt-10 mx-auto flex justify-center">
+        <button
+          onClick={() => router.push("/")}
+        >
+          <Image
+            src={HeadLogo}
+            alt="Header Logo"
+            className=" h-[36px] w-[228px]"
+          />
+        </button>
       </div>
       <div className="pt-[64px]">
         <Image src={Logo} alt="Logo" className="h-[48px] w-[48px] mx-auto" />
